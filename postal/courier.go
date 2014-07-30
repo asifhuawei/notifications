6ee1b919d531a3371630bcc318382249d237d5d4
Ryan Moran <rmoran@pivotallabs.com>
2014-07-30 14:09:28 -0700

    Moves response generator code into courier
    [#75908058]

--- postal/courier.go
+++ postal/courier.go
@@ -13,9 +13,14 @@ import (
     "github.com/pivotal-cf/uaa-sso-golang/uaa"
 )
 
+type NotifyResponse []map[string]string
+type NotifyFailureResponse map[string][]string
 type NotificationType int
 
 const (
+    StatusNotFound  = "notfound"
+    StatusNoAddress = "noaddress"
+
     IsSpace NotificationType = iota
     IsUser
 )
@@ -96,8 +101,111 @@ func (courier Courier) Dispatch(w http.ResponseWriter, rawToken,
     })
     clientID := clientToken.Claims["client_id"].(string)
 
-    responseGenerator := NewNotifyResponseGenerator(courier.logger, courier.guidGenerator, courier.mailClient)
-    responseGenerator.GenerateResponse(users, options, space, organization, clientID, w, notificationType)
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    env := config.NewEnvironment()
+    messages := NotifyResponse{}
+
+    subjectTemplate, err := courier.LoadSubjectTemplate(options.Subject, NewTemplateManager())
+    if err != nil {
+        Error(w, http.StatusInternalServerError, []string{"An email template could not be loaded"})
+        return nil
+    }
+
+    plainTextTemplate, htmlTemplate, err := courier.LoadBodyTemplates(notificationType, NewTemplateManager())
+    if err != nil {
+        Error(w, http.StatusInternalServerError, []string{"An email template could not be loaded"})
+        return nil
+    }
+
+    for userGUID, uaaUser := range users {
+        if len(uaaUser.Emails) > 0 {
+            context := NewMessageContext(uaaUser, options, env, space, organization,
+                clientID, courier.guidGenerator, plainTextTemplate, htmlTemplate, subjectTemplate)
+
+            emailStatus := courier.SendMailToUser(context, courier.logger, courier.mailClient)
+            courier.logger.Println(emailStatus)
+
+            mailInfo := make(map[string]string)
+            mailInfo["status"] = emailStatus
+            mailInfo["recipient"] = uaaUser.ID
+            mailInfo["notification_id"] = context.MessageID
+
+            messages = append(messages, mailInfo)
+        } else {
+            var status string
+            if uaaUser.ID == "" {
+                status = StatusNotFound
+            } else {
+                status = StatusNoAddress
+            }
+            mailInfo := make(map[string]string)
+            mailInfo["status"] = status
+            mailInfo["recipient"] = userGUID
+            mailInfo["notification_id"] = ""
+
+            messages = append(messages, mailInfo)
+        }
+    }
+
+    responseBytes, err := json.Marshal(messages)
+    if err != nil {
+        panic(err)
+    }
+    w.WriteHeader(http.StatusOK)
+    w.Write(responseBytes)
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
 
     return nil
+}
+
+func (courier Courier) SendMailToUser(context MessageContext, logger *log.Logger,
+    mailClient mail.ClientInterface) string {
+
+    logger.Printf("Sending email to %s", context.To)
+    status, message, err := SendMail(mailClient, context)
+    if err != nil {
+        panic(err)
+    }
+
+    logger.Print(message.Data())
+    return status
+}
+
+func (courier Courier) LoadSubjectTemplate(subject string, templateManager EmailTemplateManager) (string, error) {
+    var templateToLoad string
+    if subject == "" {
+        templateToLoad = "subject.missing"
+    } else {
+        templateToLoad = "subject.provided"
+    }
+
+    subjectTemplate, err := templateManager.LoadEmailTemplate(templateToLoad)
+    if err != nil {
+        return "", err
+    }
+
+    return subjectTemplate, nil
+}
+
+func (courier Courier) LoadBodyTemplates(notificationType NotificationType, templateManager EmailTemplateManager) (string, string, error) {
+    var plainTextTemplate, htmlTemplate string
+    var plainErr, htmlErr error
+
+    if notificationType == IsSpace {
+        plainTextTemplate, plainErr = templateManager.LoadEmailTemplate("space_body.text")
+        htmlTemplate, htmlErr = templateManager.LoadEmailTemplate("space_body.html")
+    } else {
+        plainTextTemplate, plainErr = templateManager.LoadEmailTemplate("user_body.text")
+        htmlTemplate, htmlErr = templateManager.LoadEmailTemplate("user_body.html")
+    }
+
+    if plainErr != nil {
+        return "", "", plainErr
+    }
+
+    if htmlErr != nil {
+        return "", "", htmlErr
+    }
+
+    return plainTextTemplate, htmlTemplate, nil
 }
--- postal/courier_test.go
+++ postal/courier_test.go
@@ -259,4 +259,155 @@ var _ = Describe("Courier", func() {
             })
         })
     })
+
+    Describe("SendMailToUser", func() {
+        It("logs the email address of the recipient and returns the status", func() {
+            messageContext := postal.MessageContext{
+                To: "fake-user@example.com",
+            }
+
+            mailClient = FakeMailClient{}
+
+            status := courier.SendMailToUser(messageContext, logger, &mailClient)
+
+            Expect(buffer.String()).To(ContainSubstring("Sending email to fake-user@example.com"))
+            Expect(status).To(Equal("delivered"))
+        })
+
+        It("logs the message envelope", func() {
+            messageContext := postal.MessageContext{
+                To:                     "fake-user@example.com",
+                From:                   "from@email.com",
+                Subject:                "the subject",
+                Text:                   "body content",
+                KindDescription:        "the kind description",
+                PlainTextEmailTemplate: "{{.Text}}",
+                SubjectEmailTemplate:   "{{.Subject}}",
+            }
+
+            mailClient = FakeMailClient{}
+
+            courier.SendMailToUser(messageContext, logger, &mailClient)
+
+            data := []string{
+                "From: from@email.com",
+                "To: fake-user@example.com",
+                "Subject: the subject",
+                `body content`,
+            }
+            results := strings.Split(buffer.String(), "\n")
+            for _, item := range data {
+                Expect(results).To(ContainElement(item))
+            }
+        })
+    })
+
+    Describe("LoadSubjectTemplate", func() {
+        var manager postal.EmailTemplateManager
+
+        Context("when subject is not set in the params", func() {
+            It("returns the subject.missing template", func() {
+                manager.ReadFile = func(path string) (string, error) {
+                    if strings.Contains(path, "missing") {
+                        return "the missing subject", nil
+                    }
+                    return "incorrect", nil
+                }
+
+                manager.FileExists = func(path string) bool {
+                    return false
+                }
+
+                subject := ""
+
+                subjectTemplate, err := courier.LoadSubjectTemplate(subject, manager)
+                if err != nil {
+                    panic(err)
+                }
+
+                Expect(subjectTemplate).To(Equal("the missing subject"))
+            })
+        })
+
+        Context("when subject is set in the params", func() {
+            It("returns the subject.provided template", func() {
+                manager.ReadFile = func(path string) (string, error) {
+                    if strings.Contains(path, "provided") {
+                        return "the provided subject", nil
+                    }
+                    return "incorrect", nil
+                }
+
+                manager.FileExists = func(path string) bool {
+                    return false
+                }
+
+                subject := "is provided"
+
+                subjectTemplate, err := courier.LoadSubjectTemplate(subject, manager)
+                if err != nil {
+                    panic(err)
+                }
+
+                Expect(subjectTemplate).To(Equal("the provided subject"))
+            })
+        })
+    })
+
+    Describe("LoadBodyTemplates", func() {
+        var manager postal.EmailTemplateManager
+
+        Context("loadSpace is true", func() {
+            It("returns the space templates", func() {
+
+                manager.ReadFile = func(path string) (string, error) {
+                    if strings.Contains(path, "space") && strings.Contains(path, "text") {
+                        return "space plain text", nil
+                    }
+                    if strings.Contains(path, "space") && strings.Contains(path, "html") {
+                        return "space html code", nil
+                    }
+                    return "incorrect", nil
+                }
+
+                manager.FileExists = func(path string) bool {
+                    return false
+                }
+
+                plain, html, err := courier.LoadBodyTemplates(postal.IsSpace, manager)
+                if err != nil {
+                    panic(err)
+                }
+
+                Expect(plain).To(Equal("space plain text"))
+                Expect(html).To(Equal("space html code"))
+            })
+        })
+
+        Context("loadSpace is false", func() {
+            It("returns the user templates", func() {
+                manager.ReadFile = func(path string) (string, error) {
+                    if strings.Contains(path, "user") && strings.Contains(path, "text") {
+                        return "user plain text", nil
+                    }
+                    if strings.Contains(path, "user") && strings.Contains(path, "html") {
+                        return "user html code", nil
+                    }
+                    return "incorrect", nil
+                }
+
+                manager.FileExists = func(path string) bool {
+                    return false
+                }
+
+                plain, html, err := courier.LoadBodyTemplates(postal.IsUser, manager)
+                if err != nil {
+                    panic(err)
+                }
+
+                Expect(plain).To(Equal("user plain text"))
+                Expect(html).To(Equal("user html code"))
+            })
+        })
+    })
 })
