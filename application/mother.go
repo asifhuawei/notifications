92de4179e7a41e49c527da978ed6c1ce4bcaf92e
Ryan Moran and Zachary Gershman <zgershman@pivotal.io>
2014-12-10 10:42:29 -0800

    Allows for configuration of SMTP_AUTH_MECHANISM
    - Supported types are now cram-md5, none and plain
    
    [#84306056]
    
    Conflicts:
    	application/mother.go
    	config/environment.go
    	config/environment_test.go
    	mail/client.go
    	mail/client_test.go

--- application/mother.go
+++ application/mother.go
@@ -1,143 +1,154 @@
 package application
 
 import (
-    "log"
-    "os"
+	"log"
+	"os"
 
-    "github.com/cloudfoundry-incubator/notifications/cf"
-    "github.com/cloudfoundry-incubator/notifications/config"
-    "github.com/cloudfoundry-incubator/notifications/gobble"
-    "github.com/cloudfoundry-incubator/notifications/mail"
-    "github.com/cloudfoundry-incubator/notifications/models"
-    "github.com/cloudfoundry-incubator/notifications/postal"
-    "github.com/cloudfoundry-incubator/notifications/web/handlers"
-    "github.com/cloudfoundry-incubator/notifications/web/middleware"
-    "github.com/cloudfoundry-incubator/notifications/web/services"
-    "github.com/nu7hatch/gouuid"
-    "github.com/pivotal-cf/uaa-sso-golang/uaa"
-    "github.com/ryanmoran/stack"
+	"github.com/cloudfoundry-incubator/notifications/cf"
+	"github.com/cloudfoundry-incubator/notifications/config"
+	"github.com/cloudfoundry-incubator/notifications/gobble"
+	"github.com/cloudfoundry-incubator/notifications/mail"
+	"github.com/cloudfoundry-incubator/notifications/models"
+	"github.com/cloudfoundry-incubator/notifications/postal"
+	"github.com/cloudfoundry-incubator/notifications/web/handlers"
+	"github.com/cloudfoundry-incubator/notifications/web/middleware"
+	"github.com/cloudfoundry-incubator/notifications/web/services"
+	"github.com/nu7hatch/gouuid"
+	"github.com/pivotal-cf/uaa-sso-golang/uaa"
+	"github.com/ryanmoran/stack"
 )
 
 type Mother struct {
-    logger *log.Logger
-    queue  *gobble.Queue
+	logger *log.Logger
+	queue  *gobble.Queue
 }
 
 func NewMother() *Mother {
-    return &Mother{}
+	return &Mother{}
 }
 
 func (mother *Mother) Logger() *log.Logger {
-    if mother.logger == nil {
-        mother.logger = log.New(os.Stdout, "[WEB] ", log.LstdFlags)
-    }
-    return mother.logger
+	if mother.logger == nil {
+		mother.logger = log.New(os.Stdout, "[WEB] ", log.LstdFlags)
+	}
+	return mother.logger
 }
 
 func (mother *Mother) Queue() *gobble.Queue {
-    if mother.queue == nil {
-        mother.queue = gobble.NewQueue()
-    }
-    return mother.queue
+	if mother.queue == nil {
+		mother.queue = gobble.NewQueue()
+	}
+	return mother.queue
 }
 
 func (mother Mother) NewUAARecipe() postal.UAARecipe {
-    env := config.NewEnvironment()
-    uaaClient := uaa.NewUAA("", env.UAAHost, env.UAAClientID, env.UAAClientSecret, "")
-    uaaClient.VerifySSL = env.VerifySSL
-    cloudController := cf.NewCloudController(env.CCHost, !env.VerifySSL)
+	env := config.NewEnvironment()
+	uaaClient := uaa.NewUAA("", env.UAAHost, env.UAAClientID, env.UAAClientSecret, "")
+	uaaClient.VerifySSL = env.VerifySSL
+	cloudController := cf.NewCloudController(env.CCHost, !env.VerifySSL)
 
-    tokenLoader := postal.NewTokenLoader(&uaaClient)
-    userLoader := postal.NewUserLoader(&uaaClient, mother.Logger(), cloudController)
-    spaceLoader := postal.NewSpaceLoader(cloudController)
-    templateLoader := postal.NewTemplateLoader(postal.NewFileSystem(), env.RootPath)
-    mailer := mother.Mailer()
-    receiptsRepo := models.NewReceiptsRepo()
+	tokenLoader := postal.NewTokenLoader(&uaaClient)
+	userLoader := postal.NewUserLoader(&uaaClient, mother.Logger(), cloudController)
+	spaceLoader := postal.NewSpaceLoader(cloudController)
+	templateLoader := postal.NewTemplateLoader(postal.NewFileSystem(), env.RootPath)
+	mailer := mother.Mailer()
+	receiptsRepo := models.NewReceiptsRepo()
 
-    return postal.NewUAARecipe(tokenLoader, userLoader, spaceLoader, templateLoader, mailer, receiptsRepo)
+	return postal.NewUAARecipe(tokenLoader, userLoader, spaceLoader, templateLoader, mailer, receiptsRepo)
 }
 
 func (mother Mother) EmailRecipe() postal.MailRecipeInterface {
-    env := config.NewEnvironment()
-    return postal.NewEmailRecipe(mother.Mailer(), postal.NewTemplateLoader(postal.NewFileSystem(), env.RootPath))
+	env := config.NewEnvironment()
+	return postal.NewEmailRecipe(mother.Mailer(), postal.NewTemplateLoader(postal.NewFileSystem(), env.RootPath))
 }
 
 func (mother Mother) NotificationFinder() services.NotificationFinder {
-    clientsRepo, kindsRepo := mother.Repos()
-    return services.NewNotificationFinder(clientsRepo, kindsRepo, mother.Database())
+	clientsRepo, kindsRepo := mother.Repos()
+	return services.NewNotificationFinder(clientsRepo, kindsRepo, mother.Database())
 }
 
 func (mother Mother) Mailer() postal.Mailer {
-    return postal.NewMailer(mother.Queue(), uuid.NewV4)
+	return postal.NewMailer(mother.Queue(), uuid.NewV4)
 }
 
 func (mother Mother) MailClient() *mail.Client {
-    env := config.NewEnvironment()
-    mailConfig := mail.Config{
-        User:           env.SMTPUser,
-        Pass:           env.SMTPPass,
-        Host:           env.SMTPHost,
-        Port:           env.SMTPPort,
-        TestMode:       env.TestMode,
-        SkipVerifySSL:  env.VerifySSL,
-        DisableTLS:     !env.SMTPTLS,
-        LoggingEnabled: env.SMTPLoggingEnabled,
-    }
-    client, err := mail.NewClient(mailConfig, mother.Logger())
-    if err != nil {
-        panic(err)
-    }
+	env := config.NewEnvironment()
+	mailConfig := mail.Config{
+		User:           env.SMTPUser,
+		Pass:           env.SMTPPass,
+		Host:           env.SMTPHost,
+		Port:           env.SMTPPort,
+		Secret:         env.SMTPCRAMMD5Secret,
+		TestMode:       env.TestMode,
+		SkipVerifySSL:  env.VerifySSL,
+		DisableTLS:     !env.SMTPTLS,
+		LoggingEnabled: env.SMTPLoggingEnabled,
+	}
 
-    return client
+	switch env.SMTPAuthMechanism {
+	case config.SMTPAuthNone:
+		mailConfig.AuthMechanism = mail.AuthNone
+	case config.SMTPAuthPlain:
+		mailConfig.AuthMechanism = mail.AuthPlain
+	case config.SMTPAuthCRAMMD5:
+		mailConfig.AuthMechanism = mail.AuthCRAMMD5
+	}
+
+	client, err := mail.NewClient(mailConfig, mother.Logger())
+	if err != nil {
+		panic(err)
+	}
+
+	return client
 }
 
 func (mother Mother) Repos() (models.ClientsRepo, models.KindsRepo) {
-    return models.NewClientsRepo(), mother.KindsRepo()
+	return models.NewClientsRepo(), mother.KindsRepo()
 }
 
 func (mother Mother) Logging() stack.Middleware {
-    return stack.NewLogging(mother.Logger())
+	return stack.NewLogging(mother.Logger())
 }
 
 func (mother Mother) ErrorWriter() handlers.ErrorWriter {
-    return handlers.NewErrorWriter()
+	return handlers.NewErrorWriter()
 }
 
 func (mother Mother) Authenticator(scopes ...string) middleware.Authenticator {
-    return middleware.NewAuthenticator(config.UAAPublicKey, scopes...)
+	return middleware.NewAuthenticator(config.UAAPublicKey, scopes...)
 }
 
 func (mother Mother) Registrar() services.Registrar {
-    clientsRepo, kindsRepo := mother.Repos()
-    return services.NewRegistrar(clientsRepo, kindsRepo)
+	clientsRepo, kindsRepo := mother.Repos()
+	return services.NewRegistrar(clientsRepo, kindsRepo)
 }
 
 func (mother Mother) Database() models.DatabaseInterface {
-    env := config.NewEnvironment()
-    return models.NewDatabase(env.DatabaseURL)
+	env := config.NewEnvironment()
+	return models.NewDatabase(env.DatabaseURL)
 }
 
 func (mother Mother) PreferencesFinder() *services.PreferencesFinder {
-    return services.NewPreferencesFinder(models.NewPreferencesRepo(), mother.GlobalUnsubscribesRepo(), mother.Database())
+	return services.NewPreferencesFinder(models.NewPreferencesRepo(), mother.GlobalUnsubscribesRepo(), mother.Database())
 }
 
 func (mother Mother) PreferenceUpdater() services.PreferenceUpdater {
-    return services.NewPreferenceUpdater(mother.GlobalUnsubscribesRepo(), mother.UnsubscribesRepo(), mother.KindsRepo())
+	return services.NewPreferenceUpdater(mother.GlobalUnsubscribesRepo(), mother.UnsubscribesRepo(), mother.KindsRepo())
 }
 
 func (mother Mother) KindsRepo() models.KindsRepo {
-    return models.NewKindsRepo()
+	return models.NewKindsRepo()
 }
 
 func (mother Mother) UnsubscribesRepo() models.UnsubscribesRepo {
-    return models.NewUnsubscribesRepo()
+	return models.NewUnsubscribesRepo()
 }
 
 func (mother Mother) GlobalUnsubscribesRepo() models.GlobalUnsubscribesRepo {
-    return models.NewGlobalUnsubscribesRepo()
+	return models.NewGlobalUnsubscribesRepo()
 }
 
 func (mother Mother) CORS() middleware.CORS {
-    env := config.NewEnvironment()
-    return middleware.NewCORS(env.CORSOrigin)
+	env := config.NewEnvironment()
+	return middleware.NewCORS(env.CORSOrigin)
 }
--- config/environment.go
+++ config/environment.go
@@ -1,72 +1,93 @@
 package config
 
 import (
-    "crypto/md5"
-    "errors"
-    "fmt"
-    "net/url"
-    "strings"
+	"crypto/md5"
+	"errors"
+	"fmt"
+	"net/url"
+	"strings"
 
-    "github.com/ryanmoran/viron"
+	"github.com/ryanmoran/viron"
 )
+
+const (
+	SMTPAuthNone    = "none"
+	SMTPAuthPlain   = "plain"
+	SMTPAuthCRAMMD5 = "cram-md5"
+)
+
+var SMTPAuthMechanisms = []string{SMTPAuthNone, SMTPAuthPlain, SMTPAuthCRAMMD5}
 
 var UAAPublicKey string
 
 type Environment struct {
-    CCHost             string `env:"CC_HOST"              env-required:"true"`
-    CORSOrigin         string `env:"CORS_ORIGIN"          env-default:"*"`
-    DBLoggingEnabled   bool   `env:"DB_LOGGING_ENABLED"`
-    DatabaseURL        string `env:"DATABASE_URL"         env-required:"true"`
-    EncryptionKey      string `env:"ENCRYPTION_KEY"       env-required:"true"`
-    Port               string `env:"PORT"                 env-default:"3000"`
-    RootPath           string `env:"ROOT_PATH"`
-    SMTPHost           string `env:"SMTP_HOST"            env-required:"true"`
-    SMTPLoggingEnabled bool   `env:"SMTP_LOGGING_ENABLED" env-default:"false"`
-    SMTPPass           string `env:"SMTP_PASS"`
-    SMTPPort           string `env:"SMTP_PORT"            env-required:"true"`
-    SMTPTLS            bool   `env:"SMTP_TLS"             env-default:"true"`
-    SMTPUser           string `env:"SMTP_USER"`
-    Sender             string `env:"SENDER"               env-required:"true"`
-    TestMode           bool   `env:"TEST_MODE"            env-default:"false"`
-    UAAClientID        string `env:"UAA_CLIENT_ID"        env-required:"true"`
-    UAAClientSecret    string `env:"UAA_CLIENT_SECRET"    env-required:"true"`
-    UAAHost            string `env:"UAA_HOST"             env-required:"true"`
-    VerifySSL          bool   `env:"VERIFY_SSL"           env-default:"true"`
-    VCAPApplication    struct {
-        InstanceIndex int `json:"instance_index"`
-    }   `env:"VCAP_APPLICATION" env-required:"true"`
+	CCHost             string `env:"CC_HOST"              env-required:"true"`
+	CORSOrigin         string `env:"CORS_ORIGIN"          env-default:"*"`
+	DBLoggingEnabled   bool   `env:"DB_LOGGING_ENABLED"`
+	DatabaseURL        string `env:"DATABASE_URL"         env-required:"true"`
+	EncryptionKey      string `env:"ENCRYPTION_KEY"       env-required:"true"`
+	Port               string `env:"PORT"                 env-default:"3000"`
+	RootPath           string `env:"ROOT_PATH"`
+	SMTPAuthMechanism  string `env:"SMTP_AUTH_MECHANISM"  env-required:"true"`
+	SMTPCRAMMD5Secret  string `env:"SMTP_CRAMMD5_SECRET"`
+	SMTPHost           string `env:"SMTP_HOST"            env-required:"true"`
+	SMTPLoggingEnabled bool   `env:"SMTP_LOGGING_ENABLED" env-default:"false"`
+	SMTPPass           string `env:"SMTP_PASS"`
+	SMTPPort           string `env:"SMTP_PORT"            env-required:"true"`
+	SMTPTLS            bool   `env:"SMTP_TLS"             env-default:"true"`
+	SMTPUser           string `env:"SMTP_USER"`
+	Sender             string `env:"SENDER"               env-required:"true"`
+	TestMode           bool   `env:"TEST_MODE"            env-default:"false"`
+	UAAClientID        string `env:"UAA_CLIENT_ID"        env-required:"true"`
+	UAAClientSecret    string `env:"UAA_CLIENT_SECRET"    env-required:"true"`
+	UAAHost            string `env:"UAA_HOST"             env-required:"true"`
+	VerifySSL          bool   `env:"VERIFY_SSL"           env-default:"true"`
+	VCAPApplication    struct {
+		InstanceIndex int `json:"instance_index"`
+	} `env:"VCAP_APPLICATION" env-required:"true"`
 }
 
 func NewEnvironment() Environment {
-    env := Environment{}
-    err := viron.Parse(&env)
-    if err != nil {
-        panic(err)
-    }
-    env.parseDatabaseURL()
-    env.validateEncryptionKey()
+	env := Environment{}
+	err := viron.Parse(&env)
+	if err != nil {
+		panic(err)
+	}
+	env.parseDatabaseURL()
+	env.validateEncryptionKey()
+	env.validateSMTPAuthMechanism()
 
-    return env
+	return env
 }
 
 func (env *Environment) parseDatabaseURL() {
-    databaseURL := env.DatabaseURL
-    databaseURL = strings.TrimPrefix(databaseURL, "http://")
-    databaseURL = strings.TrimPrefix(databaseURL, "https://")
-    databaseURL = strings.TrimPrefix(databaseURL, "tcp://")
-    databaseURL = strings.TrimPrefix(databaseURL, "mysql://")
-    databaseURL = strings.TrimPrefix(databaseURL, "mysql2://")
-    parsedURL, err := url.Parse("tcp://" + databaseURL)
-    if err != nil {
-        panic(errors.New(fmt.Sprintf("Could not parse DATABASE_URL %q, it does not fit format %q", env.DatabaseURL, "tcp://user:pass@host/dname")))
-    }
+	databaseURL := env.DatabaseURL
+	databaseURL = strings.TrimPrefix(databaseURL, "http://")
+	databaseURL = strings.TrimPrefix(databaseURL, "https://")
+	databaseURL = strings.TrimPrefix(databaseURL, "tcp://")
+	databaseURL = strings.TrimPrefix(databaseURL, "mysql://")
+	databaseURL = strings.TrimPrefix(databaseURL, "mysql2://")
+	parsedURL, err := url.Parse("tcp://" + databaseURL)
+	if err != nil {
+		panic(errors.New(fmt.Sprintf("Could not parse DATABASE_URL %q, it does not fit format %q", env.DatabaseURL, "tcp://user:pass@host/dname")))
+	}
 
-    password, _ := parsedURL.User.Password()
-    env.DatabaseURL = fmt.Sprintf("%s:%s@%s(%s)%s?parseTime=true", parsedURL.User.Username(), password, parsedURL.Scheme, parsedURL.Host, parsedURL.Path)
+	password, _ := parsedURL.User.Password()
+	env.DatabaseURL = fmt.Sprintf("%s:%s@%s(%s)%s?parseTime=true", parsedURL.User.Username(), password, parsedURL.Scheme, parsedURL.Host, parsedURL.Path)
 }
 
 func (env *Environment) validateEncryptionKey() {
-    key := []byte(env.EncryptionKey)
-    sum := md5.Sum(key)
-    env.EncryptionKey = string(sum[:])
+	key := []byte(env.EncryptionKey)
+	sum := md5.Sum(key)
+	env.EncryptionKey = string(sum[:])
+}
+
+func (env *Environment) validateSMTPAuthMechanism() {
+	for _, mechanism := range SMTPAuthMechanisms {
+		if mechanism == env.SMTPAuthMechanism {
+			return
+		}
+	}
+
+	panic(errors.New(fmt.Sprintf("Could not parse SMTP_AUTH_MECHANISM %q, it is not one of the allowed values: %+v", env.SMTPAuthMechanism, SMTPAuthMechanisms)))
 }
